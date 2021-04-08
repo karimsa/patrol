6fa5310abfde41a9eb37e5360b99adc9c70ac2d1
shrunbr <shrunbr@as211553.net>
2021-04-08 06:54:17 +0000

    copied build-css.sh to create an alpine compatible version

---
+++ scripts/build-css-arm64v8.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -eo pipefail
+
+function html_min() {
+    # > {{
+    # }} <
+    # }} {{
+    # > <
+    tr -d '\n' \
+        | sed -E 's/([>\}\}])[[:space:]]+([<\{\{])/\1\2/g' \
+        | tr -s ' '
+}
+
+function css_min() {
+    if test "$NODE_ENV" = "production"; then
+        postcss
+    else
+        tr -d '`'
+    fi
+}
+
+PATH="$PATH:$(dirname $0)/../node_modules/.bin"
+
+mkdir -p dist
+
+cat index.html | html_min > dist/index.html
+
+css=`mktemp`
+tailwindcss build | css_min > dist/styles.css
